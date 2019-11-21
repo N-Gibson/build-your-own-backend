c56e9d8245fd9ad4978c8c2372ce21750b8b1700
Noah Gibson <noah.gibson99@gmail.com>
2019-11-21 10:21:46 -0700

    Add tables to migrations

--- db/migrations/20191120174742_initial.js
+++ db/migrations/20191120174742_initial.js
@@ -1,8 +1,32 @@
 
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable('teams', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('venue');
+      table.string('crestUrl');
+      table.string('founded');
+
+      table.timestamp(true, true);
+    }),
+
+    knex.schema.createTable('players', (table) => {
+      table.increments('id').primary;
+      table.string('name');
+      table.string('position');
+      table.string('nationality');
+      table.string('role');
+      table.integer('shirtNumber').unsigned();
+
+      table.timestamp(true, true);
+    })
+  ])
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+    knex.schema.dropTable('teams'),
+    knex.schema.dropTable('players'),
+  ])
 };
