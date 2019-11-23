3b6e4e037b71889b355a8535f6943737d2e9bd41
Noah Gibson <noah.gibson99@gmail.com>
2019-11-22 19:48:58 -0700

    Add get endpoints to server file

--- db/seeds/dev/seeds.js
+++ db/seeds/dev/seeds.js
@@ -1,49 +1,97 @@
-const teamsData = require( '../../../data/team');
-const playersData = require('../../../data/players');
+// const teamsData = require( '../../../data/team');
+// const playersData = require('../../../data/players');
+const data = require('../../../data/data');
 
 const createTeam = (knex, team) => {
   return knex('teams')
     .insert({
       name: team.name,
+      area: team.area.name,
       venue: team.venue,
       crestUrl: team.crestUrl,
       founded: team.founded,
+      clubColors: team.clubColors,
     }, 'id')
-      .then(id => {
-        let playersPromise = [];
-        playersData
-          .filter(player => player.teams_id === team.id)
-          .forEach(player => {
-            playersPromise.push(
-              createPlayer(knex, {
-                name: player.name,
-                position: player.position,
-                nationality: player.nationality,
-                role: player.role,
-                shirtNumber: player.shirtNumber,
-                team_id: id,
-              })
-            )
+    .then(teamId => {
+      let playerPromises = [];
+      data.forEach(team => {
+        team.squad.forEach(player => {
+          playerPromises.push(createPlayer(knex, {
+            name: player.name,
+            position: player.position,
+            nationality: player.nationality,
+            shirtNumber: player.shirtNumber,
+            role: player.role,
+            team_id: teamId[0],
           })
-          return Promise.all(playersPromise)
-      })
-}
+        )
+      });
+    })
+    return Promise.all(playerPromises);
+  })
+};
 
 const createPlayer = (knex, player) => {
-  console.log(player)
   return knex('players').insert(player);
 };
 
-exports.seed = knex => {
-  return knex('players')
-    .del()
+exports.seed = (knex) => {
+  return knex('players').del()
     .then(() => knex('teams').del())
     .then(() => {
-      let teamsPromise = [];
-      teamsData.forEach(team => {
-        teamsPromise.push(createTeam(knex, team))
-      })
-      return Promise.all(teamsPromise)
+      let teamPromises = [];
+
+      data.forEach(team => {
+        teamPromises.push(createTeam(knex, team));
+      });
+      return Promise.all(teamPromises);
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(`Error seeding data: ${error}`));
 }
+
+// const createTeam = (knex, team) => {
+//   return knex('teams')
+//     .insert({
+//       name: team.name,
+//       venue: team.venue,
+//       crestUrl: team.crestUrl,
+//       founded: team.founded,
+//     }, 'id')
+//       .then(id => {
+//         let playersPromise = [];
+//         playersData
+//           .filter(player => player.teams_id === teams.id)
+//           .forEach(player => {
+//             playersPromise.push(
+//               createPlayer(knex, {
+//                 name: player.name,
+//                 position: player.position,
+//                 nationality: player.nationality,
+//                 role: player.role,
+//                 shirtNumber: player.shirtNumber,
+//                 team_id: id,
+//               })
+//             )
+//           })
+//           return Promise.all(playersPromise)
+//       })
+// }
+
+// const createPlayer = (knex, player) => {
+//   console.log(player)
+//   return knex('players').insert(player);
+// };
+
+// exports.seed = knex => {
+//   return knex('players')
+//     .del()
+//     .then(() => knex('teams').del())
+//     .then(() => {
+//       let teamsPromise = [];
+//       teamsData.forEach(team => {
+//         teamsPromise.push(createTeam(knex, team))
+//       })
+//       return Promise.all(teamsPromise)
+//     })
+//     .catch(error => console.error(error))
+// }
--- server.js
+++ server.js
@@ -15,4 +15,25 @@ app.get('/api/v1/teams', (request, response) => {
 });
 app.listen(app.get('port'), () => {
     console.log(`App is running on ${app.get('port')}`)
-})
+});
+
+app.get('/api/v1/teams', (request, response) => {
+  database('teams').select()
+    .then((teams) => {
+      response.status(200).json(teams);
+    })
+    .catch((err) => {
+      response.status(500).json({ err });
+    });
+});
+
+app.get('/api/v1/players', (request, response) => {
+  database('players').select()
+    .then((players) => {
+      response.status(200).json(players);
+    })
+    .catch((err) => {
+      response.status(500).json({ err });
+    });
+});
+
